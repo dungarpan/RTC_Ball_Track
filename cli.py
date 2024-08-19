8747d67016ac433b8290e2c5e76b03ef9cfb31f2
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2024-08-18 23:39:48 -0400

    added tracking of the ball in the client side

--- cli.py
+++ cli.py
@@ -2,14 +2,49 @@ import asyncio
 import cv2
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.signaling import TcpSocketSignaling
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Value
+import numpy as np
 
-def process_a(frame_queue):
+def process_a(frame_queue, x_value, y_value):
     """Process to handle received frames."""
     while True:
         frame = frame_queue.get()
         if frame is None:
             break
+
+        # Convert frame to HSV color space for color detection
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Define the color range for detecting the ball (green in this case)
+        lower_green = np.array([40, 70, 70])
+        upper_green = np.array([80, 255, 255])
+
+        # Create a mask for the green color
+        mask = cv2.inRange(hsv_frame, lower_green, upper_green)
+
+        # Find contours in the mask
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if contours:
+            # Find the largest contour
+            largest_contour = max(contours, key=cv2.contourArea)
+
+            # Get the bounding box of the largest contour
+            x, y, w, h = cv2.boundingRect(largest_contour)
+
+            # Calculate the center of the bounding box
+            center_x = x + w // 2
+            center_y = y + h // 2
+            print(center_x,center_y)
+
+            # Update the shared values
+            x_value.value = center_x
+            y_value.value = center_y
+
+            # Draw the bounding box and center on the frame
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
+
         cv2.imshow('Received Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -22,7 +57,9 @@ async def run_client():
     pc = RTCPeerConnection()
 
     frame_queue = Queue()
-    p = Process(target=process_a, args=(frame_queue,))
+    x_value = Value('i', 0)
+    y_value = Value('i', 0)
+    p = Process(target=process_a, args=(frame_queue,x_value,y_value))
     p.start()
 
     @pc.on('track')
@@ -33,11 +70,7 @@ async def run_client():
                 frame = await track.recv()
                 img = frame.to_ndarray(format='bgr24')
                 frame_queue.put(img)
-                #cv2.imshow('Received Video', img)
-                #if cv2.waitKey(1) & 0xFF == ord('q'):
-                #    break
 
-            #cv2.destroyAllWindows()
 
     offer = await signaling.receive()
     await pc.setRemoteDescription(offer)
